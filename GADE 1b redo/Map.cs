81156ed439a90f06da06e1f3660b7025d38d8a3c
myleslouw <56117169+myleslouw@users.noreply.github.com>
2020-09-01 00:26:11 +0200

    added some more stuff before going bed, still just the framework

---
+++ GADE 1b redo/Map.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE_1b_redo
+{
+    class Map
+    {
+        public char[] mapTiles;
+        public char hero = 'H'; //A Hero object to represent the player character
+        public char[] enemyPlacement;
+        public int mapX, mapY;
+        public int rndmNum;
+        
+    }
+}
--- GADE 1b redo/Tile.cs
+++ GADE 1b redo/Tile.cs
@@ -5,19 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GADE_1b_redo
-{ 
+{
     abstract class Tile
     {
         protected string X;
         protected string Y;
-        public enum TileType { Hero, Enemy, Gold};
-        
+        public enum TileType { Hero, Enemy, Gold };
+
 
     }
 
     class Obstacle : Tile
     {
-        
+
     }
 
     class EmptyTile : Tile
@@ -31,21 +31,27 @@ namespace GADE_1b_redo
         protected int maxHP;
         protected int damage;
         protected Tile[] characterVision;
-        public enum movement{NoMvm, Up, Down, Left, Right};
+        public enum movement { NoMvm, Up, Down, Left, Right };
 
+        protected string symbol;
 
-
-        public Character(string xPos, string yPos)
+        public Character()
+        {
+            //idk what this is for  it solved this error 
+            //There is no argument given that corresponds to the required formal parameter 'xPos' of 'Character.Character(string, string, string)'
+        }
+        public Character(string xPos, string yPos, string _symbol)
         {
             X = xPos;
             Y = yPos;
+            symbol = _symbol;
 
-            
+
         }
 
         public virtual void Attack(Character target)
         {
-            
+
         }
 
         public bool IsDead()
@@ -77,8 +83,67 @@ namespace GADE_1b_redo
 
         //}
 
+
+
     }
 
-    
+    abstract class Enemy : Character
+    {
+        protected int rndmNum;
+        protected int enemyDamage { get; set; }
+        protected int enemyHP { get; set; }
+        protected int enemyMaxHP { get; set; }
+        protected string enemySymbol { get; set; }
+
+
+        public Enemy()
+        {
+
+        }
+        public Enemy(string enemyX, string enemyY, string _enemySymbol)
+        {
+            X = enemyX;
+            Y = enemyY;
+            symbol = _enemySymbol;
+
+        }
+        public override string ToString()
+        {
+            return "EnemyClassName at [X, Y] (Amount DMG)";   //?????????
+        }
+
+    }
+
+    class Goblin : Enemy
+    {
+        public Goblin(string goblinX, string goblinY)
+        {
+            X = goblinX;
+            Y = goblinY;
+        }
+
+        //public override ReturnMove()
+        //{
+        //    return
+        //}
+    }
+
+    class Hero : Character
+    {
+        public Hero()
+        {
+
+        }
+        //public override ReturnMove()
+        //{
+        //    return
+        //}
+        public override string ToString()
+        {
+            return "Player Stats: \n HP: HP/MAX HP \n Damage: 2 \n at [X, Y]";   //?????????
+        }
+
+    }
+
 
 }
